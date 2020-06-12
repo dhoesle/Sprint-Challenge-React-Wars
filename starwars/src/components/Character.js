606a176a0fb6e7f947d2b0de3e0413e8314b6f8b
Daniel Hoesle <d1@PacBook-Mro.local>
2020-06-12 09:34:37 -0600

    pulled data from api and now have list of names

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+import React from 'react'
+
+const Character = (props) => {
+    const { characterData } = props
+    console.log("Character -> props", props)
+    return (
+        <div>
+            {characterData.name}
+        </div>
+    )
+}
+
+export default Character;
